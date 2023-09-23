480c511105b0b5f9a68ab5b27d2f09bbb77b4a2c
Crazycaleb <52386586+Crazycaleb@users.noreply.github.com>
2023-09-22 22:12:48 -0400

    Phone ringing animation (temporary)

--- Assets/MultiverseHotlineScript.cs
+++ Assets/MultiverseHotlineScript.cs
@@ -12,10 +12,15 @@ public class MultiverseHotlineScript : MonoBehaviour
     public KMBombModule Module;
     public KMBombInfo BombInfo;
     public KMAudio Audio;
+    public GameObject Phoneage;
+    public KMSelectable[] Buttonage;
 
     private int _moduleId;
     private static int _moduleIdCounter = 1;
     private bool _moduleSolved;
+    private bool wantRinging = true;
+    private float elapsed = 0f;
+    private float duration;
 
     private static string[] universes = new string[]
     {
@@ -28,5 +33,32 @@ public class MultiverseHotlineScript : MonoBehaviour
     private void Start()
     {
         _moduleId = _moduleIdCounter++;
+
+        foreach (KMSelectable button in Buttonage)
+        {
+            button.OnInteract += delegate () { /*InputPress(button);*/ StartCoroutine(RingingAnimation()); return false; };
+        }
+
     }
+    
+    private IEnumerator RingingAnimation()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            elapsed = 0f;
+            duration = 2.0f;
+            while(elapsed < duration)
+            {
+                var rotation = Quaternion.Euler(0f,(float)(Math.Sin(elapsed * 20)),0f);
+                Phoneage.transform.localRotation = rotation;
+                yield return null;
+                elapsed += Time.deltaTime;        
+            }
+            Phoneage.transform.localRotation = Quaternion.Euler(0f,0f,0f);
+            yield return new WaitForSeconds((i == 0) ? 2.0f : 0f);
+        }
+
+
+    }
+
 }
